5c9f8f8fa84fa99c7a95540a0db8176c2f0a9d84
PongpolR <pongpol.r@astartechs.com>
2024-03-14 16:33:17 +0700

    edit package.json and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,51 +19,27 @@ export const page = () => {
           <h1 className="font-NotoSans text-default-1000 text-2xl font-semibold text-center my-6">
             Sign in to your account
           </h1>
-          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-            {/* <Input className="mb-3 focus:ring-violet-300" type="email"   /> */}
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="mb-3 px-3 py-3 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:bg-indigo-100 focus:border-3 focus:outline-none focus:border-eservice-purple focus:ring-eservice-purple block w-full rounded-md sm:text-sm focus:ring-1 disabled:shadow-none font-medium"
-            />
-          </div>
-          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-            {/* <Input className="" type="password" /> */}
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="px-3 py-3 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:bg-indigo-100 focus:border-3 focus:outline-none focus:border-eservice-purple focus:ring-eservice-purple block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-eservice-purple focus:invalid:ring-eservice-purple disabled:shadow-none"
-            />
-          </div>
-          <div className="mt-5">
-            {/* <input type="checkbox" name="" id="" /> */}
-            <Checkbox />
-            <span className="font-NotoSans text-default-600 font-medium">
-              Remember me
-            </span>
-          </div>
-          <div className="mt-5">
+
+          <div className="my-2">
             <p className="font-NotoSansThai text-center font-semibold">
               พบปัญหาในการลงชื่อเข้าใช้งานโปรดติดต่อ 9960 PEA ITIL
             </p>
           </div>
           <Link href={"/page1"}>
-            <Button className="h-12 mt-12 bg-pea-theme block w-full">
+            <Button className="h-12 mt-6 bg-pea-theme block w-full">
               <p className="font-NotoSans font-medium text-lg text-white w-full">
                 Sign In
               </p>
             </Button>
           </Link>
-          <div className="mt-8 text-center">
+          {/* <div className="mt-8 text-center">
             <span className="font-NotoSans font-medium text-lg">
               New user?{" "}
             </span>
             <span className="font-NotoSans font-medium text-pea-theme text-lg cursor-pointer">
               Register
             </span>
-          </div>
+          </div> */}
         </CardBody>
       </Card>
     </div>
